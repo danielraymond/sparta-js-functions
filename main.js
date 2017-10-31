efd9be173d09b82e05d2d4f7330839015c18683c
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-10-31 12:04:56 +0000

    functions practice

---
+++ main.js
@@ -0,0 +1,57 @@
+// var result = 5 + 10;
+// var result2 = 10 + 10;
+// var result4 = 11 - 2;
+//
+// console.log(result);
+// console.log(result2);
+// console.log(result4);
+
+// This is a named funtion
+// function addNumbers() {
+//   var result = 5 + 10;
+//   console.log(result);
+// }
+
+// I'm calling the function here
+// addNumbers();
+
+// This is an anonymous function
+// var anonAddNumbers = function() {
+//    var result = 5 + 10;
+//    console.log(result);
+// }
+//
+// anonAddNumbers();
+
+// function text() {
+//   console.log("Hello World!");
+// }
+//
+// text();
+//
+// var helloWorld = function() {
+//   console.log("Hello World!");
+// }
+//
+// helloWorld();
+
+// makeNoise();
+//
+// function  makeNoise() {
+//   var firstVar = 0;
+//   var second = "string";
+//
+//   alert("how annoying")
+//
+//   if
+// }
+
+function addNumbers(number1, number2) {
+
+  return number1 + number2;
+
+}
+
+addNumbers(1, 3);
+
+var
